63ee738dc5d94b27f3c8a55d28b03cce9f668eef
MohonSaha <practicewithmohon@gmail.com>
2023-05-18 13:20:12 +0600

    create filtering api to fetch category wise data

--- src/pages/Home/ShopByCategory/ShopByCategory.jsx
+++ src/pages/Home/ShopByCategory/ShopByCategory.jsx
@@ -1,57 +1,77 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 
 const ShopByCategory = () => {
-  const [selectedTab, setSelectedTab] = useState(0);
+    const [selectedTab, setSelectedTab] = useState(0);
+    const [activeTab, setActiveTab] = useState("Unicorn")
+    const [toys, setToys] = useState([])
 
-  const handleTabSelect = (index) => {
-    setSelectedTab(index);
-  };
+    const handleTabSelect = (index) => {
+        setSelectedTab(index);
+    };
 
-  return (
-    <div className="flex justify-center mt-20">
-      <Tabs selectedIndex={selectedTab} onSelect={handleTabSelect}>
-        <TabList className="flex">
-          <Tab
-            className={`px-4 py-2 rounded-sm ${
-              selectedTab === 0 ? 'bg-blue-500 text-red-700' : 'bg-[#03BFA7] text-white'
-            }`}
-          >
-            Teddy Bear
-          </Tab>
-          <Tab
-            className={`px-4 py-2 ${
-              selectedTab === 1 ? 'bg-red-500 text-white' : 'bg-[#03BFA7] text-white'
-            }`}
-          >
-            Unicorn
-          </Tab>
+    console.log(selectedTab, activeTab);
 
-          <Tab
-            className={`px-4 py-2 ${
-              selectedTab === 2 ? 'bg-red-500 text-white' : 'bg-[#03BFA7] text-white'
-            }`}
-          >
-            Dinosaur
-          </Tab>
-        </TabList>
+    const handleTabClick = (tabName) => {
+        setActiveTab(tabName);
+      };
 
-        <TabPanel>
-          <h2>Category 1 Content</h2>
-          {/* Add your content for Category 1 here */}
-        </TabPanel>
-        <TabPanel>
-          <h2>Category 2 Content</h2>
-          {/* Add your content for Category 2 here */}
-        </TabPanel>
-        <TabPanel>
-          <h2>Category 3 Content</h2>
-          {/* Add your content for Category 3 here */}
-        </TabPanel>
-      </Tabs>
-    </div>
-  );
+    useEffect(() => {
+        fetch(`http://localhost:5000/allToys/${activeTab}`)
+        .then(res => res.json())
+        .then(data =>{
+            setToys(data)
+        })
+    }, [activeTab])
+
+
+
+
+
+    return (
+        <div className="flex justify-center mt-20">
+            <Tabs selectedIndex={selectedTab} onSelect={handleTabSelect}>
+                <TabList className="flex">
+                    <Tab
+                        onClick={() => handleTabClick("Unicorn")}
+                        className={`px-4 py-2 rounded-sm ${selectedTab === 0 ? 'bg-blue-500 text-red-700' : 'bg-[#03BFA7] text-white'
+                            }`}
+                    >
+                        Unicorn
+                    </Tab>
+                    <Tab
+                        onClick={() => handleTabClick("Teddy-bear")}
+                        className={`px-4 py-2 ${selectedTab === 1 ? 'bg-red-500 text-white' : 'bg-[#03BFA7] text-white'
+                            }`}
+                    >
+                        Teddy Bear
+                    </Tab>
+
+                    <Tab
+                        onClick={() => handleTabClick("Dinosaur")}
+                        className={`px-4 py-2 ${selectedTab === 2 ? 'bg-red-500 text-white' : 'bg-[#03BFA7] text-white'
+                            }`}
+                    >
+                        Dinosaur
+                    </Tab>
+                </TabList>
+
+                <TabPanel>
+                    <h2>Category 1 Content</h2>
+                    {/* Add your content for Category 1 here */}
+                </TabPanel>
+                <TabPanel>
+                    <h2>Category 2 Content</h2>
+                    {/* Add your content for Category 2 here */}
+                </TabPanel>
+                <TabPanel>
+                    <h2>Category 3 Content</h2>
+                    {/* Add your content for Category 3 here */}
+                </TabPanel>
+            </Tabs>
+        </div>
+    );
 };
 
 export default ShopByCategory;
