f7827e95b7457944069bd6975e9586be702d59f2
MohonSaha <practicewithmohon@gmail.com>
2023-05-19 19:54:32 +0600

    add more style in shop by category

--- src/pages/Home/ShopByCategory/ShopByCategory.jsx
+++ src/pages/Home/ShopByCategory/ShopByCategory.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 import CategoryToyCard from '../CategoryToyCard/CategoryToyCard';
+import { FaTag, FaTags, FaUserTag } from 'react-icons/fa';
 
 const ShopByCategory = () => {
     const [selectedTab, setSelectedTab] = useState(0);
@@ -30,79 +31,90 @@ const ShopByCategory = () => {
 
 
     return (
-        <div className="flex justify-center mt-20 w-full">
-            <h3>Shop By Category</h3>
-            <h1>Trending Toy Categoryes</h1>
-
-            <Tabs className=" " selectedIndex={selectedTab} onSelect={handleTabSelect}>
-                <TabList className="flex justify-center space-x-12">
-                    <Tab
-                        onClick={() => handleTabClick("Unicorn")}
-                        selectedClassName="custom-active-btn"
-                        className='px-4 py-2 custom-btn-outline'
-                    >
-                        Unicorn
-                    </Tab>
-                    <Tab
-                        onClick={() => handleTabClick("Teddy-bear")}
-                        selectedClassName="custom-active-btn"
-                        className='px-4 py-2 custom-btn-outline'
-                    >
-                        Teddy Bear
-                    </Tab>
-
-                    <Tab
-                        onClick={() => handleTabClick("Dinosaur")}
-                        selectedClassName="custom-active-btn"
-                        className='px-4 py-2 custom-btn-outline'
-                    >
-                        Dinosaur
-                    </Tab>
-                </TabList>
 
 
+        <div className=''>
+            <div className='mb-6 text-center '>
+                <h3 className='text-xl text-[#03BFA7] font-semibold'> Shop By Category</h3 >
+                <h1 className='text-5xl font-bold'>Trending Toy Categoryes</h1>
+            </div>
 
-                <TabPanel className="mt-12">
+            <div className="divider text-center mb-16 md:mx-60 mx-6"><FaTags className='text-5xl text-[#03BFA7]'></FaTags></div>
 
-                    <div className="flex gap-12">
-                        {
-                            toys.map(toy => <CategoryToyCard
-                                toy={toy}
-                                key={toy._id}
-                            ></CategoryToyCard>)
-                        }
-                    </div>
-                </TabPanel>
 
-                <TabPanel>
+            <div className="flex justify-center">
 
-                    <div className="flex gap-12">
-                        {
-                            toys.map(toy => <CategoryToyCard
-                                toy={toy}
-                                key={toy._id}
-                            ></CategoryToyCard>)
-                        }
-                    </div>
-                </TabPanel>
 
-                <TabPanel>
+                <Tabs className=" " selectedIndex={selectedTab} onSelect={handleTabSelect}>
+                    <TabList className="flex justify-center space-x-12">
+                        <Tab
+                            onClick={() => handleTabClick("Unicorn")}
+                            selectedClassName="custom-active-btn"
+                            className='px-4 py-2 custom-btn-outline'
+                        >
+                            Unicorn
+                        </Tab>
+                        <Tab
+                            onClick={() => handleTabClick("Teddy-bear")}
+                            selectedClassName="custom-active-btn"
+                            className='px-4 py-2 custom-btn-outline'
+                        >
+                            Teddy Bear
+                        </Tab>
 
-                    <div className="flex gap-12">
-                        {
-                            toys.map(toy => <CategoryToyCard
-                                toy={toy}
-                                key={toy._id}
-                            ></CategoryToyCard>)
-                        }
-                    </div>
-                </TabPanel>
+                        <Tab
+                            onClick={() => handleTabClick("Dinosaur")}
+                            selectedClassName="custom-active-btn"
+                            className='px-4 py-2 custom-btn-outline'
+                        >
+                            Dinosaur
+                        </Tab>
+                    </TabList>
 
 
 
+                    <TabPanel className="mt-12">
+
+                        <div className="flex gap-12">
+                            {
+                                toys.map(toy => <CategoryToyCard
+                                    toy={toy}
+                                    key={toy._id}
+                                ></CategoryToyCard>)
+                            }
+                        </div>
+                    </TabPanel>
+
+                    <TabPanel>
+
+                        <div className="flex gap-12">
+                            {
+                                toys.map(toy => <CategoryToyCard
+                                    toy={toy}
+                                    key={toy._id}
+                                ></CategoryToyCard>)
+                            }
+                        </div>
+                    </TabPanel>
+
+                    <TabPanel>
+
+                        <div className="flex gap-12">
+                            {
+                                toys.map(toy => <CategoryToyCard
+                                    toy={toy}
+                                    key={toy._id}
+                                ></CategoryToyCard>)
+                            }
+                        </div>
+                    </TabPanel>
 
 
-            </Tabs>
+
+
+
+                </Tabs>
+            </div>
         </div>
     );
 };
