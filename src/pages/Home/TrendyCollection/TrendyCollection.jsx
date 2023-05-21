37086e952cba84b4ba76893a1cecfb956066b31a
MohonSaha <practicewithmohon@gmail.com>
2023-05-21 12:59:35 +0600

    update trendyCollection button

--- src/pages/Home/TrendyCollection/TrendyCollection.jsx
+++ src/pages/Home/TrendyCollection/TrendyCollection.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import demo1 from '../../../assets/images/Super-trendy/demo-1.jpg'
 import demo2 from '../../../assets/images/Super-trendy/demo-2.jpg'
+import { Link } from 'react-router-dom';
 
 const TrendyCollection = () => {
     return (
@@ -10,7 +11,7 @@ const TrendyCollection = () => {
                 <div className='md:w-4/5 w-full'>
                     <h1 className='text-5xl font-semibold'>Trendy Toys For Your Kids</h1>
                     <p className='mt-6 text-lg font-semibold'>We Are An Online Toys Store With More Than 12-Years Experience On The Market! Our Shop Is Aimed To Fulfill All Parents Need And Give Your Babies A Perfect Childhood!</p>
-                    <button className='custom-primary-btn mt-4'>Show Now</button>
+                    <Link to='/allToys'><button className='custom-primary-btn mt-4'>Show Now</button></Link>
                 </div>
             </div>
 
@@ -20,7 +21,7 @@ const TrendyCollection = () => {
                 <div className='w-4/5'>
                     <h1 className='text-5xl font-semibold'>New Toys Collection For Kids</h1>
                     <p className='mt-6 text-lg font-semibold'>We Are An Online Toys Store With More Than 12-Years Experience On The Market! Our Shop Is Aimed To Fulfill All Parents Need And Give Your Babies A Perfect Childhood!</p>
-                    <button className='custom-primary-btn mt-4'>Show Now</button>
+                    <Link to='/allToys'><button className='custom-primary-btn mt-4'>Show Now</button></Link>
                 </div>
             </div>
         </div>
