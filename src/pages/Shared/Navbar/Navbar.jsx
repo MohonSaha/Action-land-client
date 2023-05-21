7a35ffa34c33696b540fb27bfb89c7a647ab8427
MohonSaha <practicewithmohon@gmail.com>
2023-05-21 22:58:23 +0600

    adding navbar padding

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -50,10 +50,10 @@ const Navbar = () => {
 
     return (
         <div>
-            <div id="navbar" className="fixed  bg-white top-0 navbar h-18 px-2 md:px-20 z-50">
+            <div id="navbar" className="fixed  bg-white top-0 navbar h-18 px-6 md:px-20 z-50">
                 <div className="navbar-start">
                     <div className="dropdown">
-                        <label onClick={() => setHide(false)} tabIndex={0} className="btn px-3 bg-white lg:hidden text-black border-white hover:text-[#03BFA7] hover:bg-white hover:border-white">
+                        <label onClick={() => setHide(false)} tabIndex={0} className="btn pl-0 pr-3 bg-white lg:hidden text-black border-white hover:text-[#03BFA7] hover:bg-white hover:border-white">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 20" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
                         <ul tabIndex={0} className={`menu menu-compact dropdown-content mt-3 p-2 shadow bg-[#d9fbf6] rounded-box w-44 ${hide ? 'hidden' : 'flex'}`}>
